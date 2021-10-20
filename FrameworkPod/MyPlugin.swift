91e594637bd422805d47876a353e4233e4e6bc61
anhnt <anhnt2@gtech.ai>
2021-10-20 16:23:15 +0700

    first commit

---
+++ FrameworkPod/MyPlugin.swift
@@ -0,0 +1,16 @@
+//
+//  MyPlugin.swift
+//  FrameworkPod
+//
+//  Created by Tùng Anh Nguyễn on 20/10/2021.
+//
+
+import Foundation
+
+public class MyPlugin {
+    public init(){}
+    
+    public func sayHello() {
+        print("say hello")
+    }
+}
