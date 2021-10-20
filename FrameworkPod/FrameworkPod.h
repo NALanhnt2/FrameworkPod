a112dfbd15dc6cb37254acea95e58ce223538c8a
anhnt <anhnt2@gtech.ai>
2021-10-20 16:09:12 +0700

    Initial Commit

---
+++ FrameworkPod/FrameworkPod.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkPod.h
+//  FrameworkPod
+//
+//  Created by Tùng Anh Nguyễn on 20/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkPod.
+FOUNDATION_EXPORT double FrameworkPodVersionNumber;
+
+//! Project version string for FrameworkPod.
+FOUNDATION_EXPORT const unsigned char FrameworkPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkPod/PublicHeader.h>
+
+
